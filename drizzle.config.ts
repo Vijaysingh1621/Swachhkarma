dccc920ca342743e0edae80f6f47f7dbe33d6801
Vijay Singh <rockcodingrookie21@gmail.com>
2025-01-13 17:30:49 +0530

    initialized

---
+++ drizzle.config.ts
@@ -0,0 +1,13 @@
+import 'dotenv/config';
+import { defineConfig } from 'drizzle-kit';
+
+export default defineConfig({
+  out: './drizzle',
+  schema: './src/utils/db/schema.ts',
+  dialect: 'postgresql',
+  dbCredentials: {
+    url: `${process.env.NEXT_PUBLIC_DB}`,
+  },
+});
+
+
